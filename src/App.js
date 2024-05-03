9464f03147eaed621188c6de119ecbbaea25ea77
wahyuda <wahyuda542@smk.belajar.id>
2024-05-03 15:34:33 +0700

    Upload Tugas 6 ReactJs

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="box">
+        <h1 className="title">Hi, Saya John</h1>
+        <hr></hr>
+        <p className="paragraph">
+          Saya dari Kelas 12 sudah berhasil menginstall React
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
